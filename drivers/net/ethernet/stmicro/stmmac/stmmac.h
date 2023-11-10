19a483ae2fda7c39f6f4c76dc94f29cb86949547
Vatte Vishnuvardhan Reddy <quic_vishnuvr@quicinc.com>
2023-11-10 13:06:52 +0530

    net: stmmac: Get cv2x_vm_id on gunyah powerup event only
    Get cv2x_vm_id only during gunyah powerup event as the
    retrieved value could be incorrect during other event
    notifications.
    
    Change-Id: I81d90acdcfb7cc66558b4b1ed253978fbb39d788
    Signed-off-by: Vatte Vishnuvardhan Reddy <quic_vishnuvr@quicinc.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac.h
+++ drivers/net/ethernet/stmicro/stmmac/stmmac.h
@@ -30,6 +30,7 @@
 #include <linux/if_ether.h>
 #include <linux/if_arp.h>
 #include <linux/icmp.h>
+#include <linux/gunyah/gh_rm_drv.h>
 
 struct stmmac_resources {
 	void __iomem *addr;
@@ -383,6 +384,7 @@ struct stmmac_priv {
 	bool wol_irq_enabled;
 	bool en_wol;
 	u32 avb_vlan_id;
+	gh_vmid_t v2x_vm_id;
 	__ETHTOOL_DECLARE_LINK_MODE_MASK(adv_old);
 };
 
