6acde0cdd7b9fec6ff8272bd82968880fc434cf6
Henry Yeh <quic_hanyyeh@quicinc.com>
2024-06-05 00:41:02 -0700

    net: stmmac: Add new serdes software reset
    In the mac2mac use case, triggering PCS_PHY_START for software reset
    cause datapath problem. So a updated soft reset function is added.
    
    Change-Id: Iae315fb3f8814e80a35956c4bb7bc553b075b33c
    Signed-off-by: Henry Yeh <quic_hanyyeh@quicinc.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-serdes.h
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-serdes.h
@@ -569,6 +569,7 @@ int qcom_ethqos_serdes_update(struct qcom_ethqos *ethqos,
 			      int interface);
 void qcom_ethqos_disable_serdes_clocks(struct qcom_ethqos *ethqos);
 int qcom_ethqos_enable_serdes_clocks(struct qcom_ethqos *ethqos);
+void qcom_ethqos_serdes_phy_soft_reset(struct qcom_ethqos *ethqos);
 void qcom_ethqos_serdes_soft_reset(struct qcom_ethqos *ethqos);
 #if IS_ENABLED(CONFIG_ETHQOS_QCOM_VER4)
 void qcom_ethqos_serdes_power_down(struct qcom_ethqos *ethqos);
@@ -593,6 +594,10 @@ void qcom_ethqos_disable_serdes_clocks(struct qcom_ethqos *ethqos)
 int qcom_ethqos_enable_serdes_clocks(struct qcom_ethqos *ethqos)
 {
 	return 0;
+}
+
+void qcom_ethqos_serdes_phy_soft_reset(struct qcom_ethqos *ethqos)
+{
 }
 
 void qcom_ethqos_serdes_soft_reset(struct qcom_ethqos *ethqos)
