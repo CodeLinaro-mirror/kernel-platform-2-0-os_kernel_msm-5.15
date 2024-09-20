a6c1655e199205958496cf930d61a712452a2e27
Prachi Arora <quic_pracaror@quicinc.com>
2024-09-19 22:19:41 -0700

    net: stmmac: Save ETH max link speed
    Save maximum speed supported by MAC, PHY, and PCS layers to be
    used by other modules.
    
    Change-Id: I5bc2a4b94060c084e6c8195532a4b919ce4ff822
    Signed-off-by: Prachi Arora <quic_pracaror@quicinc.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -1258,6 +1258,9 @@ static void stmmac_validate(struct phylink_config *config,
 		xpcs_validate(priv->hw->xpcs, supported, state);
 	if (priv->hw->qxpcs)
 		qcom_xpcs_validate(priv->hw->qxpcs, supported, state);
+
+	phy_speeds(&priv->plat->max_supported_speed, 1, supported);
+
 }
 
 static void stmmac_mac_config(struct phylink_config *config, unsigned int mode,
