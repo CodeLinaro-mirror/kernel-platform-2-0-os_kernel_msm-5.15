d0b87b4d1f55de9834e8e88d320e9d84fb70cfb5
Prashanth K <quic_prashk@quicinc.com>
2024-12-02 10:42:34 +0530

    usb: dwc3-msm: Add additional configuration while enabling DP/DM irqs
    In High-speed, while entering runtime suspend due to bus suspend,
    we configure wakeup conditions for DP interrupt as falling edge
    and DM to be in rising edge. This is because during idle (FS/HS)
    D+ is high, D- is low (J state). And resume signalling is done by
    reversing the polarity (K state) for at least 20ms.
    
    On some LE platforms, during fast suspend-resume scenarios, it's
    observed that the falling/rising edge of DP/DM is not detected
    due to the timing of suspend-resume testcases. After receiving
    the suspend siganlling from host, DWC3 and DWC3-MSM takes 1-2s
    to enter runtime suspend where the irqs are configured.
    
    suspend signal from host
    -> dwc3_msm_suspend()
    (1)	set_suspend(hsphy) - we no longer rely on phy now, will
    			     be using DP/DM irqs now for wakeup
    (2)	configure_wakeup_irq
    
    If we receive a resume signal from host in between (1) and (2),
    then we would miss the rising/falling edge, but still would be
    able to detect the level low/high triggers since it should last
    at least 20ms. Even if we miss the above 20ms timegap, we would
    get a reset interrupt from host, which should take device out of
    LPM and re-enumerate.
    
    Change-Id: Id98a40aa0110da5223b7f2dced085e1df1c18b67
    Signed-off-by: Prashanth K <quic_prashk@quicinc.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -3784,7 +3784,7 @@ static void configure_usb_wakeup_interrupts(struct dwc3_msm *mdwc, bool enable)
 		else
 			configure_usb_wakeup_interrupt(mdwc,
 				&mdwc->wakeup_irq[DM_HS_PHY_IRQ],
-				IRQ_TYPE_EDGE_FALLING, enable);
+				IRQ_TYPE_EDGE_FALLING | IRQ_TYPE_LEVEL_LOW, enable);
 
 	} else if (mdwc->hs_phy->flags & PHY_HSFS_MODE) {
 		/*
@@ -3800,7 +3800,7 @@ static void configure_usb_wakeup_interrupts(struct dwc3_msm *mdwc, bool enable)
 		else
 			configure_usb_wakeup_interrupt(mdwc,
 				&mdwc->wakeup_irq[DP_HS_PHY_IRQ],
-				IRQ_TYPE_EDGE_FALLING, enable);
+				IRQ_TYPE_EDGE_FALLING | IRQ_TYPE_LEVEL_LOW, enable);
 
 	} else {
 		/* When in host mode, with no device connected, set the HS
@@ -3812,19 +3812,20 @@ static void configure_usb_wakeup_interrupts(struct dwc3_msm *mdwc, bool enable)
 			&mdwc->wakeup_irq[DP_HS_PHY_IRQ],
 			mdwc->in_host_mode && !(mdwc->use_pwr_event_for_wakeup
 			& PWR_EVENT_HS_WAKEUP)  && !mdwc->use_eusb2_phy ?
-			(IRQF_TRIGGER_HIGH | IRQ_TYPE_LEVEL_HIGH) :
-			IRQ_TYPE_EDGE_RISING, true);
+			IRQ_TYPE_LEVEL_HIGH : IRQ_TYPE_EDGE_RISING |
+			IRQ_TYPE_LEVEL_HIGH, true);
+
 		configure_usb_wakeup_interrupt(mdwc,
 			&mdwc->wakeup_irq[DM_HS_PHY_IRQ],
 			mdwc->in_host_mode && !(mdwc->use_pwr_event_for_wakeup
 			& PWR_EVENT_HS_WAKEUP) && !mdwc->use_eusb2_phy ?
-			(IRQF_TRIGGER_HIGH | IRQ_TYPE_LEVEL_HIGH) :
-			IRQ_TYPE_EDGE_RISING, true);
+			IRQ_TYPE_LEVEL_HIGH : IRQ_TYPE_EDGE_RISING |
+			IRQ_TYPE_LEVEL_HIGH, true);
 	}
 
 	configure_usb_wakeup_interrupt(mdwc,
 		&mdwc->wakeup_irq[SS_PHY_IRQ],
-		IRQF_TRIGGER_HIGH | IRQ_TYPE_LEVEL_HIGH, enable);
+		IRQ_TYPE_LEVEL_HIGH, enable);
 	return;
 
 disable_usb_irq:
